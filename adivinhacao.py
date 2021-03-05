6cb72d1a26e59faab4f831f7943acc3d80712089
João Victor <joaovictordjardim@gmail.com>
2021-03-05 07:59:00 -0300

    Condicao elif criada

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,10 +7,18 @@ numero_secreto = 42
 # O "int()" serve para forçar que o input seja um inteiro.
 chute = int(input('Digite um número: '))
 
-if (chute == numero_secreto):
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if (acertou):
     print('\nVocê acertou!')
+elif (maior):
+    print('\nO seu chute foi maior que o número secreto!')
+elif (menor):
+    print('\nO seu chute foi menor que o número secreto!')
 else:
-    print('\nVocê Errou!')
+    print('\nNúmero inválido')
 
 print('\n************')
 print('Fim do jogo!')
