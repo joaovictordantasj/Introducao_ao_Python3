d0346eca13b19dace0d4b1b5c17c58b6f565e3e2
João Victor <joaovictordjardim@gmail.com>
2021-03-06 15:51:11 -0300

    Ultilizando o while

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,25 +1,49 @@
-print('********************************')
-print('Bem vindo ao jogo de adivinhação')
-print('********************************\n')
+from os import system
+
+system('clear')
+
+print('*****************************')
+print('     Jogo de adivinhação')
+print('*****************************\n')
 
 numero_secreto = 42
+total_de_tentativas = int(
+    input('Quantas tentativas você precisa para advinhar o número secreto: '))
+contador = total_de_tentativas
+pontuacao = 0
 
-# O "int()" serve para forçar que o input seja um inteiro.
-chute = int(input('Digite um número: '))
+while (contador > 0):
+    system('clear')
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    print('*****************************')
+    print('     Jogo de adivinhação')
+    print('*****************************\n')
 
-if (acertou):
-    print('\nVocê acertou!')
-elif (maior):
-    print('\nO seu chute foi maior que o número secreto!')
-elif (menor):
-    print('\nO seu chute foi menor que o número secreto!')
-else:
-    print('\nNúmero inválido')
+    # O "int()" serve para forçar que o input seja um inteiro.
+    print(f'Tentativa {total_de_tentativas} de {contador}!\n')
+    chute = int(input('Digite um número: '))
 
-print('\n************')
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        pontuacao += 1
+    elif (maior or menor):
+        pontuacao = pontuacao
+    #     print('\nVocê acertou!')
+    # elif (maior):
+    #     print('\nO seu chute foi maior que o número secreto!')
+    # elif (menor):
+    #     print('\nO seu chute foi menor que o número secreto!')
+    else:
+        print('\nNúmero inválido')
+
+    contador -= 1
+
+    system('clear')
+
+print('\n*****************************************************************')
 print('Fim do jogo!')
-print('************')
+print(f'Você acertou {pontuacao} vez em {total_de_tentativas} tentaivas!')
+print('*****************************************************************')
