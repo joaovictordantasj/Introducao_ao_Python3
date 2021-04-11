9ad52d880cc7ada0bdf09ef168680e7b868800bf
joaovictordantasj <joaovictordjardim@gmail.com>
2021-04-11 08:26:54 -0300

    Logica do jogo melhorada

--- adivinhacao_com_for.py
+++ adivinhacao_com_for.py
@@ -1,5 +1,6 @@
-from os import system
 import random
+from os import system
+from time import sleep
 
 system('clear')
 
@@ -8,29 +9,41 @@ print('     Jogo de adivinhação')
 print('*****************************\n')
 
 numero_secreto = random.randint(0, 50)
-total_de_tentativas = 4
+total_de_tentativas = 3
 pontuacao = 1000
 
-for rodada in range(1, total_de_tentativas):
+for rodada in range(1, total_de_tentativas + 1):
     system('clear')
-
     print('*****************************')
     print('     Jogo de adivinhação')
-    print(f'     {numero_secreto}')
+    #print(f'     {numero_secreto}')
     print('*****************************\n')
 
-    print(f'-=- Tentativa {rodada} / {total_de_tentativas - 1} -=-\n')
+    print(f'-=- Tentativa {rodada} / {total_de_tentativas} -=-\n')
     # O "int()" serve para forçar que o input seja um inteiro.
-    chute = int(input('Digite um número: '))
+    chute = int(input('Digite um número entre 0 e 50: '))
+
+    if (chute < 0 or chute > 50):
+        print('O número não pode ser menor do que 0 ou maior que 50!!!')
+        sleep(1.5)
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
     if (acertou):
+        print('Parabéns, você acertou!')
+        sleep(2)
         break
-    elif (maior or menor):
-        pontuacao = pontuacao - (chute / 2)
+    elif (menor):
+        print(f'\n{chute} é MENOR que o número secreto!')
+        pontuacao = pontuacao - (chute * 2)
+        sleep(2)
+    elif(maior):
+        print(f'\n{chute} é MAIOR que o número secreto!')
+        pontuacao = pontuacao - (chute * 2)
+        sleep(2)
     else:
         print('\nValor inválido')
 
@@ -39,6 +52,6 @@ for rodada in range(1, total_de_tentativas):
 system('clear')
 print('\n*****************************************************************')
 print('Fim do jogo!')
-print(f'Total de tentativas: {rodada} / {total_de_tentativas - 1}')
+print(f'Total de tentativas: {rodada} / {total_de_tentativas}')
 print(f'Pontuação final: {pontuacao}')
 print('*****************************************************************')
